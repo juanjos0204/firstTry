fffbbd78354afad0dfc140ef02149682f718cef7
Juanjo_munoz <juanjos0204@gmail.com>
2023-03-20 19:46:31 -0500

    envio consola visual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonListComponent } from './person-list/person-list.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent,
+    PersonListComponent
   ],
   imports: [
     BrowserModule
